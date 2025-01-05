59f0bd56a5e414a49b90d377f007236dc4dab777
Eduardo Hernández <jhernandezg1403@gmail.com>
2025-01-05 13:38:21 -0600

    Termiando Par o Impar

--- 04_ParImpar/app.js
+++ 04_ParImpar/app.js
@@ -1,1 +1,17 @@
-const 
+const num1 = document.getElementById("num-1");
+const comprobar = document.getElementById("boton")
+let comprobacion = document.getElementById("resultado");
+
+
+function parImpar(){
+    const valor = num1.value;
+    impar = valor % 2;
+    if (impar !== 0) {
+       resultado = "Tu numero es: Impar"
+    }  else {
+        resultado = "Tu núemor es: Par"
+    }
+    comprobacion.textContent = resultado;
+}
+
+comprobar.addEventListener("click", parImpar);
